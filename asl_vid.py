f09d690c4d15f08608963ecbe2935a4c604ae788
Max Loetscher <mloetscher@ordinalscience.com>
2024-03-12 05:40:19 -0500

    updated app for video recognition for baseline MNIST

--- asl_vid.py
+++ asl_vid.py
@@ -9,7 +9,7 @@ import numpy as np
 import time
 import pandas as pd
 
-model = load_model('mnist_asl_model.keras')
+model = load_model('mnist_asl_model2.keras')
 
 mphands = mp.solutions.hands
 hands = mphands.Hands()
@@ -98,84 +98,3 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
-
-
-# while True:
-#     _, frame = cap.read()
-    
-#     k = cv2.waitKey(1)
-#     if k%256 == 27:
-#         # ESC pressed
-#         print("Escape hit, closing...")
-#         break
-#     elif k%256 == 32:
-#         # SPACE pressed
-#         # SPACE pressed
-#         analysisframe = frame
-#         showframe = analysisframe
-#         cv2.imshow("Frame", showframe)
-#         framergbanalysis = cv2.cvtColor(analysisframe, cv2.COLOR_BGR2RGB)
-#         resultanalysis = hands.process(framergbanalysis)
-#         hand_landmarksanalysis = resultanalysis.multi_hand_landmarks
-#         if hand_landmarksanalysis:
-#             for handLMsanalysis in hand_landmarksanalysis:
-#                 x_max = 0
-#                 y_max = 0
-#                 x_min = w
-#                 y_min = h
-#                 for lmanalysis in handLMsanalysis.landmark:
-#                     x, y = int(lmanalysis.x * w), int(lmanalysis.y * h)
-#                     if x > x_max:
-#                         x_max = x
-#                     if x < x_min:
-#                         x_min = x
-#                     if y > y_max:
-#                         y_max = y
-#                     if y < y_min:
-#                         y_min = y
-#                 y_min -= 20
-#                 y_max += 20
-#                 x_min -= 20
-#                 x_max += 20 
-
-#         analysisframe = cv2.cvtColor(analysisframe, cv2.COLOR_BGR2GRAY)
-#         analysisframe = analysisframe[y_min:y_max, x_min:x_max]
-#         analysisframe = cv2.resize(analysisframe,(28,28))
-
-
-#         nlist = []
-#         rows,cols = analysisframe.shape
-#         for i in range(rows):
-#             for j in range(cols):
-#                 k = analysisframe[i,j]
-#                 nlist.append(k)
-        
-#         datan = pd.DataFrame(nlist).T
-#         colname = []
-#         for val in range(784):
-#             colname.append(val)
-#         datan.columns = colname
-
-#         pixeldata = datan.values
-#         pixeldata = pixeldata / 255
-#         pixeldata = pixeldata.reshape(-1,28,28,1)
-
-
-# prediction = model.predict(pixeldata)
-# predarray = np.array(prediction[0])
-# letter_prediction_dict = {letterpred[i]: predarray[i] for i in range(len(letterpred))}
-# predarrayordered = sorted(predarray, reverse=True)
-# high1 = predarrayordered[0]
-# high2 = predarrayordered[1]
-# high3 = predarrayordered[2]
-# for key,value in letter_prediction_dict.items():
-#     if value==high1:
-#         print("Predicted Character 1: ", key)
-#         print('Confidence 1: ', 100*value)
-#     elif value==high2:
-#         print("Predicted Character 2: ", key)
-#         print('Confidence 2: ', 100*value)
-#     elif value==high3:
-#         print("Predicted Character 3: ", key)
-#         print('Confidence 3: ', 100*value)
-# time.sleep(5)
